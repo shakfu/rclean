0ba1c9725b9251421b68a8b60104c58baa5dfecb
Shakeeb Alireza <shakeeb.alireza@rezayat.net>
2025-07-26 16:14:43 +0300

    snap

--- src/lib.rs
+++ src/lib.rs
@@ -11,11 +11,313 @@ use std::path::Path;
 use walkdir::WalkDir;
 
 // --------------------------------------------------------------------
-// core
+// schema
+
+pub trait Job {
+    fn scan(&mut self);
+    fn remove_targets(&self);
+    fn remove_target(&self, target: &walkdir::DirEntry);
+    fn run(&mut self);
+}
+
+// pub struct Sequencer<T: Job> {
+//     pub jobs: Vec<T>,
+// }
+
+// impl<T> Default for Sequencer<T>
+// where
+//     T: Job,
+// {
+//     fn default() -> Self {
+//         Self {
+//             jobs: vec![]
+//         }
+//     }
+// }
+
+// impl<T> Sequencer<T>
+// where
+//     T: Job,
+// {
+//     pub fn run(&mut self) {
+//         for job in self.jobs.iter_mut() {
+//             job.run();
+//         }
+//     }
+// }
+
+// --------------------------------------------------------------------
+// globjob
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct GlobJob {
+    pub path: String,
+    pub patterns: Vec<String>,
+    pub dry_run: bool,
+    #[serde(skip_serializing, skip_deserializing)]
+    targets: Vec<walkdir::DirEntry>,
+    #[serde(skip_serializing, skip_deserializing)]
+    size: u64,
+    #[serde(skip_serializing, skip_deserializing)]
+    counter: i32,
+}
+
+/// Default values for a globjob instance
+impl Default for GlobJob {
+    /// default values for a globjob instance
+    fn default() -> Self {
+        Self {
+            path: ".".to_string(),
+            patterns: vec![],
+            dry_run: true,
+            targets: Vec::new(),
+            size: 0,
+            counter: 0,
+        }
+    }
+}
+
+/// implements globjob
+impl GlobJob {
+    /// constructor
+    pub fn new(
+        path: String,
+        patterns: Vec<String>,
+        dry_run: bool,
+    ) -> Self {
+        Self {
+            path,
+            patterns,
+            dry_run,
+            targets: Vec::new(),
+            size: 0,
+            counter: 0,
+        }
+    }    
+}
+
+/// Implements Job trait
+impl Job for GlobJob {
+
+    #[time("info")]
+    fn scan(&mut self) {
+        // path cases
+        let path = Path::new(&self.path);
+        let current_path = Path::new(".");
+        let parent_path = Path::new("..");
+
+        let mut builder = GlobSetBuilder::new();
+        for pattern in self.patterns.iter() {
+            builder.add(Glob::new(pattern).unwrap());
+        }
+        let gset = builder.build().unwrap();
+        for entry in WalkDir::new(path).into_iter().filter_map(|e| e.ok()) {
+            let entry_path = entry.path();
+            // silently handle "." || ".." cases
+            if entry_path == current_path || entry_path == parent_path {
+                continue;
+            }
+            // skip paths which startwith ".."
+            if entry_path.starts_with("..") {
+                warn!("skipping {:?}", entry_path.display());
+                continue;
+            }
+
+            if gset.is_match(entry_path) {
+                match entry.path().metadata() {
+                    Ok(_info) => self.size += get_size(entry_path).unwrap(),
+                    Err(e) => eprintln!("metadata not found: {e:?}"),
+                }
+                self.counter += 1;
+                self.targets.push(entry.clone());
+                info!("Matched: {:?}", entry_path.display());
+            }
+        }
+
+        info!(
+            "Matched {} item(s) totalling {:.2} MB",
+            self.counter,
+            (self.size as f64) / 1000000.
+        );
+    }
+
+    /// remove collected targets
+    fn remove_targets(&self) {
+        for name in self.targets.iter() {
+            if !self.dry_run {
+                self.remove_target(name);
+            }
+        }
+    }
+
+    /// remove file or directory
+    fn remove_target(&self, target: &walkdir::DirEntry) {
+        let p = target.path();
+        let _target = target.metadata().unwrap();
+        if _target.is_file() {
+            fs::remove_file(p).expect("could not remove file: {p}");
+        } else if _target.is_dir() {
+            fs::remove_dir_all(p).expect("could not remove directory: {p}");
+        } else {
+            warn!("skipping unknowm: {:?}", target.path().display());
+        }
+    }
+
+    /// run job
+    fn run(&mut self) {
+        self.scan();
+        self.remove_targets();
+    }
+}
+
+
+// --------------------------------------------------------------------
+// cleanbrokensymlinksjob
 
 /// Main configuration object for cleaning jobs with partial
 /// with selective (de)serialization
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct CleanBrokenSymlinksJob {
+    pub path: String,
+    pub dry_run: bool,
+    #[serde(skip_serializing, skip_deserializing)]
+    targets: Vec<walkdir::DirEntry>,
+    #[serde(skip_serializing, skip_deserializing)]
+    size: u64,
+    #[serde(skip_serializing, skip_deserializing)]
+    counter: i32,
+}
+
+/// Default values for a cleanbrokensymlinksjob instance
+impl Default for CleanBrokenSymlinksJob {
+    /// default values for a cleaningjob instance
+    fn default() -> Self {
+        Self {
+            path: ".".to_string(),
+            dry_run: true,
+            targets: Vec::new(),
+            size: 0,
+            counter: 0,
+        }
+    }
+}
+
+/// CleanBrokenSymlinksJob methods
+impl CleanBrokenSymlinksJob {
+    /// constructor
+    pub fn new(
+        path: String,
+        dry_run: bool,
+    ) -> Self {
+        Self {
+            path,
+            dry_run,
+            targets: Vec::new(),
+            size: 0,
+            counter: 0,
+        }
+    }
+}
+
+impl Job for CleanBrokenSymlinksJob {
+    /// run the cleaning job
+    #[time("info")]
+    fn scan(&mut self) {
+        // path cases
+        let path = Path::new(&self.path);
+        let current_path = Path::new(".");
+        let parent_path = Path::new("..");
+
+        for entry in WalkDir::new(path).into_iter().filter_map(|e| e.ok()) {
+            let entry_path = entry.path();
+            // silently handle "." || ".." cases
+            if entry_path == current_path || entry_path == parent_path {
+                continue;
+            }
+            // skip paths which startwith ".."
+            if entry_path.starts_with("..") {
+                warn!("skipping {:?}", entry_path.display());
+                continue;
+            }
+
+            // check if path is a dead symlink
+            if entry_path.is_symlink() {
+                // an error means it's broken
+                if let Err(_e) = fs::metadata(entry_path) {
+                    self.counter += 1;
+                    self.targets.push(entry.clone());
+                    info!("Matched broken symlink: {:?}", entry_path.display());
+                }
+            }
+        }
+
+        if !self.targets.is_empty() {
+            let confirmation = Confirm::new()
+                .with_prompt("Do you want to delete the above?")
+                .interact()
+                .unwrap();
+
+            if confirmation {
+                self.remove_targets();
+            } else {
+                warn!("Cleaning operation cancelled.");
+                return;
+            }
+        }
+
+        if !self.dry_run {
+            info!(
+                "Deleted {} item(s) totalling {:.2} MB",
+                self.counter,
+                (self.size as f64) / 1000000.
+            );
+        }
+    }
+
+    /// remove collected targets
+    fn remove_targets(&self) {
+        for name in self.targets.iter() {
+            if !self.dry_run {
+                self.remove_target(name);
+            }
+        }
+    }
+
+    /// remove file or directory with some safety measures
+    fn remove_target(&self, target: &walkdir::DirEntry) {
+        let p = target.path();
+        let _target = target.metadata().unwrap();
+        if _target.is_symlink() {
+            fs::remove_file(p).expect("could not remove symlink: {p}");
+        } 
+    }
+
+    /// run job
+    fn run(&mut self) {
+        self.scan();
+        self.remove_targets();
+    }
+}
+
+#[allow(unused_variables)]
+#[allow(dead_code)]
+fn run_seq() {
+    let gjob = GlobJob::default();
+    let sjob = CleanBrokenSymlinksJob::default();
+    let mut jobs: Vec<Box<dyn Job>> = vec![Box::new(gjob), Box::new(sjob)];
+    // jobs.push(Box::new(gjob));
+    // jobs.push(Box::new(sjob));
+    for job in jobs.iter_mut() {
+            job.run();
+        }
+}
+
+// --------------------------------------------------------------------
+// cleaningjob
+
+/// Main configuration object for cleaning jobs with partial
+/// with selective (de)serialization
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct CleaningJob {
     pub path: String,
     pub patterns: Vec<String>,
@@ -118,13 +420,13 @@ impl CleaningJob {
                     Err(e) => eprintln!("metadata not found: {e:?}"),
                 }
                 if entry.path_is_symlink() && !self.include_symlinks {
-                    continue
+                    continue;
                 }
                 self.counter += 1;
                 if self.skip_confirmation {
                     self.remove_entry(&entry);
                     info!("Deleted: {:?}", entry_path.display());
-                } 
+                }
                 self.targets.push(entry.clone());
                 info!("Matched: {:?}", entry_path.display());
             }
@@ -156,7 +458,7 @@ impl CleaningJob {
     /// remove collected targets
     pub fn remove_targets(&self) {
         for name in self.targets.iter() {
-            if !self.dry_run {                
+            if !self.dry_run {
                 self.remove_entry(name);
             }
         }
